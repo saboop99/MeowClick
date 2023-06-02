9c38e1cc3e579f5ee236507f5f8b8feac2ea2950
Laboratório de Informática <rafa4021@hotmail.com>
2023-06-01 21:51:17 -0300

    comecei a fazer o menu
    quase todos os cod prontos

---
+++ Assets/Scripts/StopMeow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StopMeow : MonoBehaviour
+{
+    //public string sceneName;
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
