6b20cf59f79ccc7178321daa47953280a3643dd2
saboop99 <rafa4021@hotmail.com>
2023-06-03 18:18:11 -0300

    controle de touch e retirada do menu

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+    GameObject particle;
+
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray))
+                {
+                    // Create a particle if hit
+                    Instantiate(particle, transform.position, transform.rotation);
+                }
+            }
+        }
+    }
+}
